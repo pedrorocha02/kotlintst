2b97591b5d8377f20ea38adf568b15f602fb209a
José Rocha <a31707619@fam.ulusiada.pt>
2023-03-21 20:31:52 +0000

    Update com Logo, Nome e Splash Screen

---
+++ app/src/main/java/com/example/projetomei/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.projetomei
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT: Long = 3000 // 3 seconds
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
+
+
